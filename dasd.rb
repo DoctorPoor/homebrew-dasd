872666899fb0b3470ba035ae5f8a581753338542
perry <8948878+DoctorPoor@users.noreply.github.com>
2022-05-15 13:01:56 -0400

    Updated to v0.1.0

--- dasd.rb
+++ dasd.rb
@@ -1,8 +1,8 @@
 class Dasd < Formula
   desc "Simple command-line tool to generate passwords"
   homepage "https://github.com/DoctorPoor/dasd"
-  url "https://github.com/DoctorPoor/dasd/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "7c4141101de9459c84b56a992fd54a3d8dfb97005bba50f48e4e2d1a9f784225"
+  url "https://github.com/DoctorPoor/dasd/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "6add866af2316e2e13fb7e6b0543fd0e942e0b657be13726a758ae10208ffcae"
   license "MIT"
   head "https://github.com/DoctorPoor/dasd.git"
 
